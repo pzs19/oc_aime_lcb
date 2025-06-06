7f116ced278a1afb7cac80e25cb25e213e21f669
panzhuoshi <pzs23@mails.tsinghua.edu.cn>
2025-06-06 13:52:52 +0800

    fix bugs

--- sh/eval.sh
+++ sh/eval.sh
@@ -7,8 +7,8 @@ MODE=$1
 if [ -z "$MODE" ]; then
     MODE="all"
 fi
-# export CUDA_VISIBLE_DEVICES=6,7
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=6,7
+# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 N_GPU=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
 echo "N_GPU: $N_GPU"
@@ -38,7 +38,7 @@ opencompass -r \
     -w outputs/reasoning \
     --datasets aime2024_gen_wwpp aime2025_gen_wwpp lcb_gen_wwpp \
     --models aime_lcb_models_api \
-    --summarizer aimc_lcb \
+    --summarizer aime_lcb \
     --max-num-worker $N_GPU \
     --dump-eval-details True \
     --mode $MODE
